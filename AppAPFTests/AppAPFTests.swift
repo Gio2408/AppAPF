6b6cbfb079d5bcf53873334c14a4d6e0067a3e8a
Gioacchino Augello <gioacchino.augello02@gmail.com>
2025-02-07 16:26:54 +0100

    Initial Commit

---
+++ AppAPFTests/AppAPFTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppAPFTests.swift
+//  AppAPFTests
+//
+//  Created by Gioacchino Augello on 07/02/25.
+//
+
+import Testing
+@testable import AppAPF
+
+struct AppAPFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
