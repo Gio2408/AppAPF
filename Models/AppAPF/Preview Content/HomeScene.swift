df19bdd491370a12e17c370bdd2cef9e4c73f439
Mike is cooked <michelevto@gmail.com>
2025-02-10 20:00:11 +0100

    Implementazione sistema di errori

---
+++ Models/AppAPF/Preview Content/HomeScene.swift
@@ -0,0 +1,15 @@
+//
+//  HomeScene.swift
+//  AppAPF
+//
+//  Created by Gioacchino Augello on 08/02/25.
+//
+import SpriteKit
+
+class HomeScene: SKScene{
+    var road = SKSpriteNode()
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
---
+++ Models/RoadJunctionError.swift
@@ -0,0 +1,8 @@
+//
+//  RoadJunctionError.swift
+//  AppAPF
+//
+//  Created by Michele Vassallo Todaro on 10/02/25.
+//
+
+import Foundation
