2418b89440c54da0424675fb75b48bf95614b5a2
Gioacchino Augello <gioacchino.augello02@gmail.com>
2025-02-12 20:07:34 +0100

    quizButton linked to quizView

--- AppAPF/Views/ContentView.swift
+++ AppAPF/Views/ContentView.swift
@@ -5,15 +5,21 @@ struct ContentView: View {
 
     @StateObject var scoreManager = ScoreManager(score: QuizScore(quiz: "Quiz Incroci", totalScore: 8, totalAnswers: 10), currentScore: 0, quizManager: QuizManager())
     @StateObject var errorManager = ErrorManager()
+    @StateObject var quizManager = QuizManager() // Aggiunto per evitare il crash
     @State private var selectedTab = 1
-    @State private var isInLevelScene = false  // Controllo per LevelScene
+    @State private var isInLevelScene = false
+    @State private var isInQuizView = false
 
     var body: some View {
         ZStack {
             if isInLevelScene {
-                LevelSceneView(isInLevelScene: $isInLevelScene)  // Mostra LevelScene a tutto schermo
+                LevelSceneView(isInLevelScene: $isInLevelScene)
+            } else if isInQuizView {
+                QuizView(isInQuizView: $isInQuizView)
+                    .environmentObject(quizManager)  // Passa il QuizManager
+                    .environmentObject(scoreManager) // Passa ScoreManager
+                    .environmentObject(errorManager) // Passa ErrorManager
             } else {
-                // Mostra la vista attuale in base alla selezione
                 Group {
                     switch selectedTab {
                     case 0:
@@ -27,10 +33,21 @@ struct ContentView: View {
                 .transition(.opacity)
                 .ignoresSafeArea()
                 
-                // Mostra la barra solo se NON siamo in LevelScene
-                if !isInLevelScene {
-                    VStack {
-                        Spacer()
+                VStack {
+                    if selectedTab == 1 { // Mostra il tasto solo quando siamo nella HomeScene
+                            Button(action: {
+                                isInQuizView = true
+                            }) {
+                                Image("quizButton")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 200, height: 80)
+                            }
+                            .padding(.top, 50)
+                        }
+                    Spacer()
+                    
+                    if !isInLevelScene {
                         HStack {
                             CustomTabButton(icon: "trophy.fill", tag: 0, selectedTab: $selectedTab)
                             CustomTabButton(icon: "house.fill", tag: 1, selectedTab: $selectedTab)
@@ -47,6 +64,7 @@ struct ContentView: View {
         }
     }
 }
+
 #Preview {
     ContentView()
 }
--- AppAPF/Views/QuizView.swift
+++ AppAPF/Views/QuizView.swift
@@ -1,12 +1,12 @@
-//Il tutto è da tradurre in inglese
 import SwiftUI
 
 struct QuizView: View {
+    @Binding var isInQuizView: Bool
     @EnvironmentObject var errorManager: ErrorManager
     @EnvironmentObject var scoreManager: ScoreManager
     @EnvironmentObject var quizManager: QuizManager
     
-    @State private var score: Int = 0 // Conta gli errori all'inizio, poi calcolerà il punteggio finale
+    @State private var score: Int = 0
     @State private var selectedAnswer: String?
     @State private var errorMessage: String?
     
@@ -19,123 +19,114 @@ struct QuizView: View {
     let quizTurns = [
         QuizTurn(question: "If your car battery dies, you can jump-start the engine using cables connected to another car’s battery.", correctAnswer: "T", imageName: "image1"),
         QuizTurn(question: "You should only change the engine oil when the warning light on the dashboard turns on.", correctAnswer: "F", imageName: "image2"),
-        QuizTurn(question: "Braking distance increases when driving on wet roads compared to dry conditions.", correctAnswer: "T", imageName: "image3"),
-        QuizTurn(question: "At an intersection with no signs or traffic lights, the driver coming from the right has the right of way.", correctAnswer: "T", imageName: "image1"),
-        QuizTurn(question: "Winter tires are mandatory across all regions of Italy from November 15 to April 15.", correctAnswer: "F", imageName: "image2"),
-        QuizTurn(question: "You can use high beams in city areas to improve visibility.", correctAnswer: "F", imageName: "image3"),
-        QuizTurn(question: "If your engine overheats, you should immediately open the radiator to release the steam.", correctAnswer: "F", imageName: "image1"),
-        QuizTurn(question: "While driving, you must keep both hands on the steering wheel at all times, except when using essential controls like turn signals or the gear shift.", correctAnswer: "T", imageName: "image2"),
-        QuizTurn(question: "Using the handbrake while driving at high speed can help you stop the car faster in an emergency.", correctAnswer: "F", imageName: "image3"),
-        QuizTurn(question: "If you have a tire blowout while driving, you should immediately brake as hard as possible to stop the car.", correctAnswer: "F", imageName: "image1")
+        QuizTurn(question: "Braking distance increases when driving on wet roads compared to dry conditions.", correctAnswer: "T", imageName: "image3")
     ]
     
     func checkAnswer(turn: QuizTurn) {
         if selectedAnswer != turn.correctAnswer {
-            let error = QuizError(question: turn.question, correctAnswer: turn.correctAnswer, userAnswer: selectedAnswer ?? "Nessuna risposta")
+            let error = QuizError(question: turn.question, correctAnswer: turn.correctAnswer, userAnswer: selectedAnswer ?? "N/A")
             errorManager.addError(error)
-            errorMessage = "Risposta errata! "
-            score += 1 // Conta gli errori
+            errorMessage = "Wrong answer!"
+            score += 1
         } else {
-            errorMessage = "Risposta corretta! "
+            errorMessage = "Correct answer!"
         }
         quizManager.currentPhaseIndex += 1
         
-        // Verifica se tutte le domande sono state fatte
         if quizManager.currentPhaseIndex == quizTurns.count {
-            // Calcola il punteggio finale quando il quiz è completo
-            let finalScore = quizTurns.count - score // Risposte corrette
-            scoreManager.score.totalScore = finalScore // Aggiorna il punteggio finale
+            let finalScore = quizTurns.count - score
+            scoreManager.score.totalScore = finalScore
         }
     }
 
     var body: some View {
-        VStack {
-            // Visualizza l'immagine sopra la domanda
-            if quizManager.currentPhaseIndex < quizTurns.count {
-                let turn = quizTurns[quizManager.currentPhaseIndex]
-                
-                Image(turn.imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 250, height: 250)
-                    .padding(.top, 30)
-                
-                // Visualizza la domanda al centro
-                Text(turn.question)
-                    .font(.title)
-                    .fontWeight(.medium)
-                    .multilineTextAlignment(.center)
-                    .padding(.top, 20)
-                    .padding(.horizontal, 30)
-                
-                // Pulsanti sotto la domanda
+        NavigationStack {
+            VStack {
+                // Back button in the top-left corner using NavigationLink
                 HStack {
-                    Button(action: {
-                        selectedAnswer = "T"
-                        checkAnswer(turn: turn)
-                    }) {
-                        Text("Vero")
-                            .font(.headline)
-                            .padding()
-                            .frame(width: 120, height: 50)
-                            .background(Color.green)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
+                    NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true)) {
+                        HStack {
+                            Image(systemName: "arrow.left")
+                                .font(.title)
+                            Text("Back")
+                                .font(.headline)
+                        }
+                        .padding()
+                        .foregroundColor(.blue)
                     }
-                    .padding()
-
-                    Button(action: {
-                        selectedAnswer = "F"
-                        checkAnswer(turn: turn)
-                    }) {
-                        Text("Falso")
-                            .font(.headline)
-                            .padding()
-                            .frame(width: 120, height: 50)
-                            .background(Color.red)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                    }
-                    .padding()
+                    Spacer()
                 }
-                
-                // Mostra il messaggio di errore o successo
-                if let errorMessage = errorMessage {
-                    Text(errorMessage)
-                        .font(.subheadline)
-                        .foregroundColor(errorMessage == "Risposta corretta! " ? .green : .red)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 10)
+                .padding(.leading, 10)
+
+                if quizManager.currentPhaseIndex < quizTurns.count {
+                    let turn = quizTurns[quizManager.currentPhaseIndex]
+
+                    Image(turn.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 250)
+                        .padding(.top, 30)
+
+                    Text(turn.question)
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .multilineTextAlignment(.center)
                         .padding(.top, 20)
+                        .padding(.horizontal, 30)
+
+                    HStack {
+                        Button(action: {
+                            selectedAnswer = "T"
+                            checkAnswer(turn: turn)
+                        }) {
+                            Text("True")
+                                .font(.headline)
+                                .padding()
+                                .frame(width: 120, height: 50)
+                                .background(Color.green)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        }
+                        .padding()
+
+                        Button(action: {
+                            selectedAnswer = "F"
+                            checkAnswer(turn: turn)
+                        }) {
+                            Text("False")
+                                .font(.headline)
+                                .padding()
+                                .frame(width: 120, height: 50)
+                                .background(Color.red)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        }
+                        .padding()
+                    }
+
+                    if let errorMessage = errorMessage {
+                        Text(errorMessage)
+                            .font(.subheadline)
+                            .foregroundColor(errorMessage == "Correct answer!" ? .green : .red)
+                            .padding(.top, 20)
+                    }
+                } else {
+                    let finalScore = quizTurns.count - score
+                    Text("You completed the quiz! Score: \(finalScore)/\(quizTurns.count)")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.green)
+                        .padding(.top, 30)
                 }
-            } else {
-                // Visualizza il punteggio finale quando il quiz è finito
-                let finalScore = quizTurns.count - score // Calcolo punteggio finale
-                Text("Hai completato il quiz! Punteggio: \(finalScore)/\(quizTurns.count)")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.green)
-                    .padding(.top, 30)
             }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 10)
         }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(10)
-        .shadow(radius: 10)
     }
-}
-
-#Preview {
-    // Crea l'istanza di QuizManager
-    let quizManager = QuizManager()
-    quizManager.currentPhaseIndex = 0 // Impostiamo la fase iniziale
-
-    // Crea l'istanza di QuizScore
-    let score = QuizScore(quiz: "Quiz Incroci", totalScore: 0)
-
-    // Passa QuizManager all'inizializzatore di ScoreManager
-    return QuizView()
-        .environmentObject(ScoreManager(score: score, currentScore: 0, quizManager: quizManager))
-        .environmentObject(quizManager) // Aggiungi QuizManager all'ambiente
-        .environmentObject(ErrorManager()) // Se ErrorManager è necessario
 }
